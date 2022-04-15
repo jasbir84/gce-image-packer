05d2806b6c6faefd825a475621fe399a4639322a
seubpong.mon <pjame.fb@gmail.com>
2022-04-15 07:40:38 +0700

    Added kubectl

--- scripts/setup.bash
+++ scripts/setup.bash
@@ -21,5 +21,7 @@ sudo systemctl enable docker
 sudo curl -L "https://github.com/docker/compose/releases/download/1.25.4/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
 sudo chmod +x /usr/local/bin/docker-compose
 
+sudo snap install kubectl --classic
+
 sudo groupadd docker
 sudo usermod -aG docker devops
